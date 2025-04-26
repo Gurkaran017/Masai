98a8900896424626710d3f251ff23695b4c81722
Gurkaran <gurkaransingh017gh@gmail.com>
2025-04-26 11:23:42 +0530

    Add files via upload

---
+++ Q12-Counter-Step-Control/q12-counter-step-control/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Counter from './components/Counter';
+
+function App() {
+  return (
+    <div style={{ padding: '20px' }}>
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
